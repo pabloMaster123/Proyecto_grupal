9f0f6cb5e5f3358a7d4e40b787a586b7376f2334
logftth <49349104+logftth@users.noreply.github.com>
2021-11-27 17:10:54 -0500

    Se terminaron varios servicios en el area de producto

--- negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/ProductoServicioImpl.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/ProductoServicioImpl.java
@@ -4,37 +4,62 @@ import co.edu.uniquindio.proyecto.entidades.Categoria;
 import co.edu.uniquindio.proyecto.entidades.Compra;
 import co.edu.uniquindio.proyecto.entidades.Producto;
 import co.edu.uniquindio.proyecto.entidades.Usuario;
+import co.edu.uniquindio.proyecto.repositorios.ProductoRepo;
+import co.edu.uniquindio.proyecto.repositorios.UsuarioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductoServicioImpl implements ProductoServicio{
 
+    @Autowired
+    private ProductoRepo productoRepo;
 
     @Override
     public Producto publicarProducto(Producto p) throws Exception {
-        return null;
-    }
+        Optional<Producto> buscado = productoRepo.findById(p.getCodigo());
+        if (buscado.isPresent()){
+            throw new Exception("El codigo del producto ya existe");
+        }
+
+        return productoRepo.save(p);    }
 
     @Override
     public Producto modificarProducto(Producto p) throws Exception {
-        return null;
+        Optional<Producto> buscado = productoRepo.findById(p.getCodigo());
+        if (buscado.isPresent()){
+            throw new Exception("El codigo del usuario no existe");
+        }
+
+        return productoRepo.save(p);
     }
 
     @Override
     public void eliminarProducto(String codigo) throws Exception {
+        Optional<Producto> buscado = productoRepo.findById(codigo);
+        if (buscado.isEmpty()){
+            throw new Exception("El codigo del producto no existe");
+        }
 
+        productoRepo.deleteById(codigo);
     }
 
     @Override
     public Producto obtenerProducto(String codigo) throws Exception {
-        return null;
-    }
+        Optional<Producto> buscado = productoRepo.findById(codigo);
+
+        if (buscado.isEmpty()){
+            throw new Exception("El codigo del producto no existe");
+        }
+
+        return  buscado.get();    }
 
     @Override
     public List<Producto> listaProductos(Categoria categoria) {
-        return null;
+        return productoRepo.findAll();
     }
 
     @Override
