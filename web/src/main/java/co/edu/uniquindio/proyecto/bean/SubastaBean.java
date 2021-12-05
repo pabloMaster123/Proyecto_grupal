c694cea9469deac0c60d9bb5a9776501f7d73ef5
logftth <49349104+logftth@users.noreply.github.com>
2021-12-04 23:31:04 -0500

    Se creo la pagina perfil y se configuro la subasata

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/SubastaBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/SubastaBean.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.proyecto.bean;
 
 import co.edu.uniquindio.proyecto.entidades.Producto;
+import co.edu.uniquindio.proyecto.entidades.Subasta;
 import co.edu.uniquindio.proyecto.entidades.Usuario;
+import co.edu.uniquindio.proyecto.servicios.CiudadServicio;
+import co.edu.uniquindio.proyecto.servicios.ProductoServicio;
+import co.edu.uniquindio.proyecto.servicios.UsuarioServicio;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,7 +19,18 @@ import java.util.List;
 @ViewScoped
 public class SubastaBean {
 
+    @Getter
+    @Setter
+    private Producto producto;
 
+    private final UsuarioServicio usuarioServicio;
+
+    private final ProductoServicio productoServicio;
+
+    public SubastaBean(UsuarioServicio usuarioServicio,ProductoServicio productoServicio) {
+        this.usuarioServicio  = usuarioServicio;
+        this.productoServicio = productoServicio;
+    }
 
     @Value("#{seguridadBean.usuarioSesion}")
     private Usuario usuarioSesion;
@@ -23,6 +38,12 @@ public class SubastaBean {
     @Getter
     @Setter
     private List<Producto> listaProductos;
+
+    @Getter
+    @Setter
+    private List<Subasta> listaSubastas;
+
+
 
 
 
