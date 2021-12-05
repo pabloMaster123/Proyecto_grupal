ed3345c52600c5814372f844a8cc8246e559f256
logftth <49349104+logftth@users.noreply.github.com>
2021-12-04 20:51:48 -0500

    Se creo la clase admi servicio

--- Persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/AdministradorRepo.java
+++ Persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/AdministradorRepo.java
@@ -1,7 +1,6 @@
 package co.edu.uniquindio.proyecto.repositorios;
 
 import co.edu.uniquindio.proyecto.entidades.Administrador;
-import co.edu.uniquindio.proyecto.entidades.Usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,5 +15,8 @@ public interface AdministradorRepo extends JpaRepository<Administrador,String> {
     Optional<Administrador> findByEmail(String email);
 
     Page<Administrador> findAll(Pageable paginador);
+
+    Optional<Administrador> findByEmailAndAndPassword(String email, String password);
+
 
 }
