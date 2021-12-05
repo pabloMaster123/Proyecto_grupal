6954add47704104934b67fc9deff2ddfaeaabcfd
logftth <49349104+logftth@users.noreply.github.com>
2021-12-04 20:12:35 -0500

    Se creo la clase admi servicio

--- negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/AdministardorServicio.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/AdministardorServicio.java
@@ -1,6 +1,11 @@
 package co.edu.uniquindio.proyecto.servicios;
 
+import co.edu.uniquindio.proyecto.entidades.Administrador;
+import co.edu.uniquindio.proyecto.entidades.Usuario;
+
 public interface AdministardorServicio {
+
+    Administrador login(String email, String pasword) throws Exception;
 
 
 
